55639e5269cf60abb73f43c3acd8fa6605cfd508
Mamun <mamun.cse63@gmail.com>
2020-10-24 08:29:32 +0600

    Bootswatch-Lux theme, npm pakages(react-bootstrap, react-router-bootstrap, react-router-dom), navbar, different pages route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome To ESHOP</h1>
+        </Container>
+      </main>
+    </Router>
   )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Nav, Navbar, Container } from 'react-bootstrap'
+
+// Wrapping the Link to 'LinkContainer' to prevent loading page, just like react-router-dom 'Link'
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>EShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <LinkContainer to='/about'>
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/products'>
+                <Nav.Link>Products</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/stats'>
+                <Nav.Link>Stats</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/add'>
+                <Nav.Link>Add_Products</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/update'>
+                <Nav.Link>Update_Products</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
